08b6a67cd94debe3782b08bfc192f8708490cf92
Luis Felipe <filipinho_v3@hotmail.com>
2024-09-07 22:04:30 -0300

    test: StudentServiceTest getAllStudent() and saveStudent() tests implemented!

--- src/test/java/com/project/test_student/StudentServiceTest/StudentServiceTest.java
+++ src/test/java/com/project/test_student/StudentServiceTest/StudentServiceTest.java
@@ -10,16 +10,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.mockito.ArgumentMatchers.any;
+
+
 public class StudentServiceTest {
 
     @InjectMocks
     private StudentService studentService;
+    @Mock
     private StudentRepository studentRepository;
     private Student student;
     private StudentDto studentDto;
@@ -60,7 +65,6 @@ public class StudentServiceTest {
 
         student.setHoursRequired(hoursRequiredTotal);
         student.setCourses(courses);
-
     }
 
     @Test
@@ -80,6 +84,7 @@ public class StudentServiceTest {
     void givenStudentDtoObject_whenSaveStudentDto_thenReturnStudent(){
 
         //GIVEN
+        BDDMockito.given(studentRepository.save(any(Student.class))).willReturn(student);
         BDDMockito.given(studentService.saveStudent(studentDto)).willReturn(student);
 
         //WHEN
@@ -89,5 +94,28 @@ public class StudentServiceTest {
         Assertions.assertThat(std).isEqualTo(student);
         Assertions.assertThat(std.getName()).isEqualTo(student.getName());
         Assertions.assertThat(std.getCourses()).isEqualTo(student.getCourses());
+    }
+
+    @Test
+    void givenStudentList_whenFindAll_thenReturnStudentList(){
+
+        //GIVEN
+        Student studentTemp = new Student();
+        studentTemp.setName("marcos");
+        studentTemp.setId("1923129");
+        studentTemp.setHoursAttended(1925L);
+
+        studentRepository.saveAll(List.of(studentTemp, student));
+
+        BDDMockito.given(studentRepository.findAll()).willReturn(List.of(studentTemp, student));
+        BDDMockito.given(studentService.getAllStudent()).willReturn(List.of(studentTemp, student));
+
+        //WHEN
+        List<Student> allStudent = studentService.getAllStudent();
+
+        //THEN
+        Assertions.assertThat(allStudent).isEqualTo(List.of(studentTemp, student));
+        Assertions.assertThat(allStudent.get(0).getName()).isEqualTo(studentTemp.getName());
+        Assertions.assertThat(allStudent.get(1).getHoursAttended()).isEqualTo(student.getHoursAttended());
     }
 }
